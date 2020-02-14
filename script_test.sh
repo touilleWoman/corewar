53a5cbc7b6865b1a4577937a914b3c1039dc1853
Nabih ALI <naali@student.42.fr>
2020-02-14 07:52:19 +0100

    epuration des dossier

--- script_test.sh
+++ script_test.sh
@@ -6,13 +6,13 @@
 #    By: nabih <naali@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/01 07:20:49 by nabih             #+#    #+#              #
-#    Updated: 2020/02/11 20:46:24 by nabih            ###   ########.fr        #
+#    Updated: 2020/02/14 07:48:40 by naali            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 ./asm $1.s
 hexdump $1.cor > mon_resultat.txt
-./asm_justin $1.s
+./asm_zaz $1.s
 hexdump $1.cor > zaz_resultat.txt
 diff -u mon_resultat.txt zaz_resultat.txt
 rm mon_resultat.txt zaz_resultat.txt
