5acf1e5a50fc53a84818a70166f1e1e0848a2757
Jie LEBLOND <jleblond@e1r5p8.42.fr>
2020-02-27 18:35:05 +0100

    accepte champ without comment

--- tests/test_vm.py
+++ tests/test_vm.py
@@ -1,9 +1,11 @@
-from hypothesis import given, strategies as st
+# from hypothesis import given, strategies as st
 import subprocess
 import re
 import random
 from pathlib import Path
 
+
+base_dir = Path(__file__).absolute().parent.parent
 
 
 # TEST_CHAMP = ['./champs/zork.cor', './champs/toto.cor']
@@ -12,52 +14,44 @@ from pathlib import Path
 def get_all_champs():
     """compile and get all the *.cor """
     # import pdb; pdb.set_trace()
-    base_dir = Path(__file__).absolute().parent.parent
     subprocess.run(["make"], cwd=base_dir)
     s_path = Path(base_dir / "champs").glob("**/*.s")
     all_s = [x for x in s_path]
     for x in all_s:
         subprocess.run([f'{base_dir}/asm', str(x)])
     #all champs
-    champs_path = Path(base_dir / "champs").glob("**/*.cor")
-
+    # champs_path = Path(base_dir / "champs").glob("**/*.cor")
 
     #one champ OK
     # champs_path = Path(base_dir / "champs").glob("**/turtle.cor")
     # champs_path = Path(base_dir / "champs").glob("**/bee_gees.cor")
 
     #one champs ERROR
-    # champs_path = Path(base_dir / "champs").glob("**/toto.cor")
+    champs_path = Path(base_dir / "champs").glob("**/toto.cor")
 
-
-
-
-    all_champs = list(champs_path)
+    all_champs = [str(i) for i in champs_path]
     return all_champs
 
 
 def get_my_dump(dump_cyle, test_champs):
-    command_lst = ["./corewar", *test_champs, "-dump", str(dump_cyle)]
+    command = ["./corewar", *test_champs, "-dump", str(dump_cyle)]
     dump = subprocess.run(
-        command_lst, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=base_dir
     )
 
     return dump.stdout.decode("utf-8")
 
 
 def get_zaz_dump(dump_cyle, test_champs):
-    command_lst = ["./champs/zaz_corewar", *test_champs, "-d",str(dump_cyle) ]
+    command = ["./champs/zaz_corewar", *test_champs, "-d", str(dump_cyle)]
     dump = subprocess.run(
-        command_lst, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=base_dir
     )
     return dump.stdout.decode("utf-8")
 
 
 
-def strip_useless(src):
-    # string = re.sub(r"0x\w{4} : ", "", string)
-    # string = re.sub("\n", "", string)
-
+def reformat_zaz_dump(src):
     ret = []
     for l in src.split('\n'):
         match = re.match(r"^0x([0-9a-f]{4}) :(( [0-9a-f]{2}){32})(( [0-9a-f]{2}){32}) $", l)
@@ -76,12 +70,12 @@ def strip_useless(src):
 def get_the_2_dump(dump_cyle, test_champs):
     my_dump = get_my_dump(dump_cyle, test_champs)
     zaz_dump = get_zaz_dump(dump_cyle, test_champs)
-    zaz_dump = strip_useless(zaz_dump)
+    zaz_dump = reformat_zaz_dump(zaz_dump)
     return (my_dump, zaz_dump)
 
 
 
-
+# test if my dump result is the same with the zaz_vm
 @given(
     dump_cyle=st.integers(min_value=0, max_value=5000),
     test_champs=st.sets(st.sampled_from(get_all_champs()), min_size=1, max_size=4),
@@ -91,3 +85,30 @@ def test_dump_output(dump_cyle, test_champs):
     my_dump, zaz_dump = get_the_2_dump(dump_cyle, test_champs)
     assert my_dump == zaz_dump
 
+
+
+def get_my_cycle(test_champs):
+    command = ["./corewar", *test_champs, '-v', '|', 'grep', 'Contestant', '-2']
+    end_cycle = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    return dump.stdout.decode("utf-8")
+
+# # @given(
+# #     test_champs=st.sets(st.sampled_from(get_all_champs()), min_size=1, max_size=4),
+# # )
+# def test_end_cycle(test_champs):
+#     command = ["./corewar", *test_champs, '-v']
+
+#     out = subprocess.run(
+#         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=base_dir
+#     )
+#     match = re.search(r'It is now cycl', out)
+#     # match = re.search(r'It is now cycle \d*\nContestant', out)
+#     if match:
+#         my_end_cycle = int(re.search(r'\d*', match))
+#         print(my_end_cycle)
+#         assert my_end_cycle == 1000
+
+# test_end_cycle(['./champs/zork.cor'])
