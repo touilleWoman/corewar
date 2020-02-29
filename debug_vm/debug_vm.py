e6e2ace5e639634785c6bdf3f991d09aa76b08b7
Jie LEBLOND <jleblond@e1r6p17.42.fr>
2020-02-29 19:49:26 +0100

    better organisation of debug_vm.py

--- debug_vm/debug_vm.py
+++ debug_vm/debug_vm.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from declare_c_struct import C_struct
 from colorama import Fore
 
+
 def get_vm_lib():
     try:
         # make dylib
@@ -22,84 +23,80 @@ def get_vm_lib():
         exit()
 
 
-def user_input(stop, p_vm, vm_lib):
-    u_input = input('-->')
-    if u_input == 'n':
+def user_input(stop, p_c_struct, vm_lib):
+    u_input = input("-->")
+    if u_input == "n":
         stop += 1
     elif u_input.isdigit() and int(u_input) > stop:
         stop = int(u_input)
-    elif u_input == "q" :
-        vm_lib.free_vm(p_vm)
+    elif u_input == "q":
+        vm_lib.free_vm(p_c_struct)
         exit()
-    else :
-        print("usage:\n"
-                    "\tn: next_cycle\n"
-                    "\tnb : go to cycle ...\n"
-                    "\tq: quit\n"
-                )
-        user_input(stop, p_vm, vm_lib)
+    else:
+        print("usage:\n" "\tn: next_cycle\n" "\tnb : go to cycle ...\n" "\tq: quit\n")
+        user_input(stop, p_c_struct, vm_lib)
     return stop
 
+
 def print_mem(arena):
-    for i in range(0,4096):
+    for i in range(0, 4096):
         if i % 32 == 0:
-            print(Fore.GREEN + "0x%04x : "% i, end='');
+            print(Fore.GREEN + "0x%04x : " % i, end="")
         if (arena[i]) != 0:
-            print(Fore.RED + "%02x"% (arena[i]), end=' ')
+            print(Fore.RED + "%02x" % (arena[i]), end=" ")
         else:
-            print(Fore.WHITE + "%02x"% (arena[i]), end=' ')
-        if (i + 1) % 32 == 0 :
+            print(Fore.WHITE + "%02x" % (arena[i]), end=" ")
+        if (i + 1) % 32 == 0:
             print()
 
-def loop(vm, p_vm, vm_lib):
-    stop = 0
-    while vm.cursor_nb:
-        while vm.delta_cycle_counter < vm.cycle_to_die:
-            if vm.cycle_total >= stop :
-                # vm_lib.dump_mem(vm.arena)
-                print_mem(vm.arena)
 
-                print('cycle at: ', vm.cycle_total)
-                print('cycle_to_die:', vm.cycle_to_die)
-                print('processus_nb: ', vm.cursor_nb)
-                print('live_nb between check: ', vm.live_counter, '\n\n')
-                stop = user_input(stop, p_vm, vm_lib)
+class Vm(object):
+    """docstring for Vm"""
 
-            if vm_lib.one_round(p_vm) == False:
-                return
-        vm_lib.check(p_vm)
-    vm_lib.declare_winner(p_vm)
+    def __init__(self):
+        self.vm_lib = get_vm_lib()
+        self.c_struct = C_struct()
+        self.p_c_struct = ctypes.pointer(self.c_struct)
+
+    def loop(self, stop=0):
+        while self.c_struct.cursor_nb:
+            while self.c_struct.delta_cycle_counter < self.c_struct.cycle_to_die:
+                if self.c_struct.cycle_total >= stop:
+                    print_mem(self.c_struct.arena)
+                    print("cycle at: ", self.c_struct.cycle_total)
+                    print("cycle_to_die:", self.c_struct.cycle_to_die)
+                    print("processus_nb: ", self.c_struct.cursor_nb)
+                    print("live_nb between check: ", self.c_struct.live_counter, "\n\n")
+                    stop = user_input(stop, self.p_c_struct, self.vm_lib)
+
+                if self.vm_lib.one_round(self.p_c_struct) == False:
+                    return
+            self.vm_lib.check(self.p_c_struct)
+        self.vm_lib.declare_winner(self.p_c_struct)
+
+    def run_vm(self):
+        if len(sys.argv) < 2:
+            self.vm_lib.usage()
+        else:
+            LP_c_char = ctypes.POINTER(ctypes.c_char)
+            LP_LP_c_char = ctypes.POINTER(LP_c_char)
+            self.vm_lib.init_vm(self.p_c_struct)
+            argc = len(sys.argv)
+            argv = (LP_c_char * (argc + 1))()
+            for i, arg in enumerate(sys.argv):
+                enc_arg = arg.encode("utf-8")
+                argv[i] = ctypes.create_string_buffer(enc_arg)
+            if self.vm_lib.parse(
+                self.p_c_struct, argc, argv
+            ) and self.vm_lib.init_cursor_lst(self.p_c_struct):
+                self.vm_lib.player_to_arena(self.p_c_struct)
+                self.loop()
+            self.vm_lib.free_vm(self.p_c_struct)
 
 
 def main_vm():
-    # get all my C functions for virtual machine
-    vm_lib = get_vm_lib()
-
-    if len(sys.argv) < 2:
-        vm_lib.usage()
-    else:
-        # c_struct is the main structure for all the c functions
-        c_struct = C_struct()
-
-        # create pointer of c_struct for C functions
-        p_c_struct = ctypes.pointer(c_struct)
-
-        vm_lib.init_vm(p_c_struct)
-
-        # convert sys.argv to c type argv
-        LP_c_char = ctypes.POINTER(ctypes.c_char)
-        LP_LP_c_char = ctypes.POINTER(LP_c_char)
-        argc = len(sys.argv)
-        argv = (LP_c_char * (argc + 1))()
-        for i, arg in enumerate(sys.argv):
-            enc_arg = arg.encode("utf-8")
-            argv[i] = ctypes.create_string_buffer(enc_arg)
-
-        if vm_lib.parse(p_c_struct, argc, argv) and vm_lib.init_cursor_lst(p_c_struct):
-            vm_lib.player_to_arena(p_c_struct)
-            loop(c_struct, p_c_struct, vm_lib)
-
-        vm_lib.free_vm(p_c_struct)
+    vm = Vm()
+    vm.run_vm()
 
 
 if __name__ == "__main__":
--- debug_vm/declare_c_struct.py
+++ debug_vm/declare_c_struct.py
@@ -7,7 +7,7 @@
 from ctypes import *
 
 
-MEM_SIZE = 4*1024
+MEM_SIZE = 4 * 1024
 PROG_NAME_LENGTH = 128
 COMMENT_LENGTH = 2048
 CHAMP_MAX_SIZE = int(MEM_SIZE / 6)
@@ -24,40 +24,46 @@ class Cursor(Structure):
 
 
 class Cursor(Structure):
-    _fields_ = [("regs", c_uint32 * (REG_NUMBER + 1)),
-    ("live_counter",c_uint),
-    ("c_id", c_uint),
-    ("wait_cycle", c_int),
-    ("no_live_cycle", c_int),
-    ("pc", c_int16),
-    ("op", c_uint8),
-    ("carry", Bool),
-    ("next", POINTER(Cursor)),
-    ("prev", POINTER(Cursor))]
+    _fields_ = [
+        ("regs", c_uint32 * (REG_NUMBER + 1)),
+        ("live_counter", c_uint),
+        ("c_id", c_uint),
+        ("wait_cycle", c_int),
+        ("no_live_cycle", c_int),
+        ("pc", c_int16),
+        ("op", c_uint8),
+        ("carry", Bool),
+        ("next", POINTER(Cursor)),
+        ("prev", POINTER(Cursor)),
+    ]
 
 
 class Players(Structure):
-    _fields_ = [("prog_name", c_char * (PROG_NAME_LENGTH + 1)),
-    ("comment", (c_char) * (COMMENT_LENGTH + 1)),
-    ("prog_data", (c_uint8) * (CHAMP_MAX_SIZE)),
-    ("file_size", c_uint),
-    ("file", POINTER(c_uint8)),
-    ("prog_size",c_int),
-    ("live_counter",c_int),
-    ("player_id",c_int8)]
+    _fields_ = [
+        ("prog_name", c_char * (PROG_NAME_LENGTH + 1)),
+        ("comment", (c_char) * (COMMENT_LENGTH + 1)),
+        ("prog_data", (c_uint8) * (CHAMP_MAX_SIZE)),
+        ("file_size", c_uint),
+        ("file", POINTER(c_uint8)),
+        ("prog_size", c_int),
+        ("live_counter", c_int),
+        ("player_id", c_int8),
+    ]
 
 
 class C_struct(Structure):
-    _fields_ = [("players", Players * MAX_PLAYERS),
-    ("arena", c_uint8 * MEM_SIZE),
-    ("dump", c_int),
-    ("player_nb", c_uint8),
-    ("cycle_total", c_int),
-    ("cycle_to_die", c_int),
-    ("max_check_counter", c_int),
-    ("delta_cycle_counter", c_int),
-    ("live_counter", c_int),
-    ("Cursor", POINTER(Cursor)),
-    ("cursor_nb", c_int),
-    ("winner", c_int),
-    ("flags", c_char)]
+    _fields_ = [
+        ("players", Players * MAX_PLAYERS),
+        ("arena", c_uint8 * MEM_SIZE),
+        ("dump", c_int),
+        ("player_nb", c_uint8),
+        ("cycle_total", c_int),
+        ("cycle_to_die", c_int),
+        ("max_check_counter", c_int),
+        ("delta_cycle_counter", c_int),
+        ("live_counter", c_int),
+        ("Cursor", POINTER(Cursor)),
+        ("cursor_nb", c_int),
+        ("winner", c_int),
+        ("flags", c_char),
+    ]
