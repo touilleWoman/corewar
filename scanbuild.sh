ed53490d97191215d1bf1c87ac14fe4d2d5fadf9
Charles COUTURE <chcoutur@e1r6p15.42.fr>
2020-02-29 17:30:12 +0100

    Anihilation des fichiers hors ASM bis

--- scanbuild.sh
+++
@@ -1,6 +0,0 @@
- curl -o checker-279.tar.bz2 https://clang-analyzer.llvm.org/downloads/checker-279.tar.bz2
- tar -jxf checker-279.tar.bz2
- rm -rf checker-279.tar.bz2
- mv checker-279 static_analyzer
- echo "alias scan='$PWD/static_analyzer/bin/scan-build'" >> ~/.zshrc
- source ~/.zshrc
